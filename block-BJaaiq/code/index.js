af6c7160b5cbd53f6c14ff62dfd309d28b8a202a
Abhijeet Mane <abhi@Varshas-MacBook-Air.local>
2022-01-01 19:24:42 +0530

    Added change in Assignment on Infinite Scroll

--- block-BJaaiq/code/index.js
+++ block-BJaaiq/code/index.js
@@ -1,35 +1,34 @@
 let add = document.querySelector(".add");
 
+let max = 10;
 let lastIndex = 0;
 
-function addQuotes() {
-    if (lastIndex === quotes.length - 1) return;
-  
-    let windowRelativeBottom = document.documentElement.getBoundingClientRect().bottom;
+function addQuotes() {  
+    let scrollTop = document.documentElement.scrollTop;
+    let scrollHeight = document.documentElement.scrollHeight;
+    let clientHeight = document.documentElement.clientHeight;
 
-    if (windowRelativeBottom > document.documentElement.clientHeight + 100) return;
-
-    renderItem(lastIndex + 1);
+    if (clientHeight + scrollTop >= scrollHeight && lastIndex < quotes.length - 1) {
+        renderItem();
+    }
 }
-  
 window.addEventListener("scroll", addQuotes); 
 
+function renderItem() {
+    for(let i = 0; i < max; i++){
+        let h2 = document.createElement("h2");
+        h2.innerText = quotes[lastIndex].quoteText;
+        let h3 = document.createElement("h3");
+        h3.innerText = quotes[lastIndex].quoteAuthor;
 
-function renderItem(i) {
-    let h2 = document.createElement("h2");
-    h2.innerText = quotes[i].quoteText;
-    let h3 = document.createElement("h3");
-    h3.innerText = quotes[i].quoteAuthor;
-
-    add.append(h2,h3);
-    lastIndex = i;
+        add.append(h2,h3);
+        lastIndex++;
+    }
 }
 
 function ready() {
-    alert(`The content of the DOM is loaded`)
-    for(let i = 0; i < 3; i++){
-        renderItem(i);
-    }
+    alert(`The content of the DOM is loaded`);
+    addQuotes();
 }
 
 document.addEventListener("DOMContentLoaded", ready);
