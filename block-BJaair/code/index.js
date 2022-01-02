0e9de9a452f42e7380633d2adc30b28f3ca1dc05
Abhijeet Mane <abhi@Varshas-MacBook-Air.local>
2022-01-02 11:16:16 +0530

    Added change in Notice board Assignment

--- block-BJaair/code/index.js
+++ block-BJaair/code/index.js
@@ -2,28 +2,79 @@ let root = document.querySelector(".cardHolder");
 let form = document.querySelector(".container");
 let cardInput = document.querySelector(".cardInput");
 
-let index = 0;
-function createInput(value1, value2) {
-    let li = document.createElement("li")
-    let input1 = document.createElement("input");
-    input1.classList.add("cardInput");
-    input1.value = value1;
-    let input2 = document.createElement("input");
-    input2.classList.add("cardInput");
-    input2.value = value2;
+let data = JSON.parse(localStorage.getItem("notice")) || [];
 
-    li.append(input1, input2);
-    root.append(li);
+function updateData(item) {
+    localStorage.setItem("notice", JSON.stringify(item));
+    createInput(data);
+}
+
+function handleInput(event, item ,id) {
+    if(event.keyCode === 13){
+        let updatedValue = event.target.value;
+        data[id][item] = updatedValue;
+        updateData(data);
+    }   
+}
+
+function handleEdit(event, item ,id){
+
+    let elm = event.target;
+    let value = event.target.innerText;
+    let input = document.createElement("input");
+    input.value = value;
+
+    input.addEventListener("keyup", (event)=> {handleInput(event, item ,id)});
+    input.addEventListener("blur", (event)=> {
+        let updatedValue = event.target.value;
+        data[id][item] = updatedValue;
+        updateData(data);
+    });
+    let parent = elm.parentElement;
+    parent.replaceChild(input, elm)
+}
+
+function handleDelete(event, index){    
+    data.splice(index, 1);
+    updateData(data);
+}
+
+function createInput(arr = data) {
+    root.innerHTML = "";
+
+    let fragment = new DocumentFragment;
+    arr.forEach((elm, index)=> {
+        let li = document.createElement("li");
+        let category = document.createElement("p");
+        category.innerText = elm.category;
+        category.addEventListener("dblclick", (event)=> {handleEdit(event, "category" ,index)});
+
+        let title = document.createElement("h2");
+        title.innerText = elm.title;
+        title.addEventListener("dblclick", (event)=> {handleEdit(event, "title" ,index)});
+
+        let btn = document.createElement("button");
+        btn.innerText = "❌";
+        btn.addEventListener("click", (event)=> { handleDelete(event, index)});
+
+        li.append(category, title,btn);
+        fragment.append(li);
+    })
+    root.append(fragment);
 }
 
 function handleSubmit(event) {
     event.preventDefault();
-    let value1 = event.target.elements.input1.value;
-    let value2 = event.target.elements.input2.value;
-    index++;
-    createInput(value1, value2);
+
+    let category = event.target.elements.input1.value;
+    let title = event.target.elements.input2.value;
+    
+    data.push({category, title});
+    event.target.elements.input1.value = "";
+    event.target.elements.input2.value = "";
+    updateData(data);
 }
 
 form.addEventListener("submit", handleSubmit);
 
-
+updateData(data);
